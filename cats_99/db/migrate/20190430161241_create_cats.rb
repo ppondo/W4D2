bd9b0187e8b691099b1b7ab747914e7a6134985b
App Academy Student <patrickpondo7@gmail.com>
2019-04-30 13:27:57 -0400

    Make initial commit

---
+++ cats_99/db/migrate/20190430161241_create_cats.rb
@@ -0,0 +1,11 @@
+class CreateCats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cats do |t|
+      t.date :birth_date, null: false
+      t.string :color
+      t.string :name
+      t.string :sex
+      t.timestamps  
+    end
+  end
+end
